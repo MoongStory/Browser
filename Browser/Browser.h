e80f131ebb0a74cf278d8646e9e7766cd3b2048d
Moong <moongstory@gmail.com>
2022-04-30 13:07:57 +0900

    namespace 범위 변경

--- Browser/Browser.h
+++ Browser/Browser.h
@@ -14,27 +14,24 @@
 
 namespace MOONG
 {
-	namespace BROWSER
+	class Browser
 	{
-		class Browser
-		{
-		public:
-		protected:
-		private:
-			static const std::string REG_SUB_KEY_WINDOWS_DEFAULT_BROWSER;
-			static const std::string REG_VALUE_WINDOWS_DEFAULT_BROWSER;
-
-		public:
-			static int OpenURL(const std::string url, std::string browser = "Default");
-			static int OpenURLWithWindowsDefaultBrowser(const std::string url);
-			static int OpenURLWithIE(const std::string url);
-			static int OpenURLWithChrome(const std::string url);
-			static int OpenURLWithMSEdge(const std::string url);
-
-			static LSTATUS getWindowsDefaultBrowser(std::string& windows_default_browser);
-		protected:
-		private:
-		};
-	}
+	public:
+	protected:
+	private:
+		static const std::string REG_SUB_KEY_WINDOWS_DEFAULT_BROWSER;
+		static const std::string REG_VALUE_WINDOWS_DEFAULT_BROWSER;
+		
+	public:
+		static int OpenURL(const std::string url, std::string browser = "Default");
+		static int OpenURLWithWindowsDefaultBrowser(const std::string url);
+		static int OpenURLWithIE(const std::string url);
+		static int OpenURLWithChrome(const std::string url);
+		static int OpenURLWithMSEdge(const std::string url);
+		
+		static LSTATUS getWindowsDefaultBrowser(std::string& windows_default_browser);
+	protected:
+	private:
+	};
 }
 #endif	// _BROWSER_H_
--- Browser/Test.cpp
+++ Browser/Test.cpp
@@ -3,21 +3,21 @@
 int main()
 {
 	// -kiosk 옵션의 경우 해당 브라우저가 하나도 실행중이지 않아야 키오스크 모드로 실행된다.
-	MOONG::BROWSER::Browser::OpenURL("www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURL("www.google.com", "edge");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURL("-kiosk www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURLWithChrome("www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURLWithChrome("-kiosk www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURLWithIE("www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURLWithMSEdge("www.google.com");
-	system("pause");
-	MOONG::BROWSER::Browser::OpenURLWithMSEdge("-kiosk www.google.com");
+	MOONG::Browser::OpenURL("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURL("www.google.com", "edge");
+	//system("pause");
+	//MOONG::Browser::OpenURL("-kiosk www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithChrome("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithChrome("-kiosk www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithIE("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithMSEdge("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithMSEdge("-kiosk www.google.com");
 
 	return EXIT_SUCCESS;
 }
