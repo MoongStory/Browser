3bdb5886742e4cdef7f2bed9b0da57a9f68b459e
Moong <moongstory@gmail.com>
2022-11-09 23:44:32 +0900

    불필요한 예외처리 제거

--- Browser/Browser.cpp
+++ Browser/Browser.cpp
@@ -16,79 +16,65 @@
 const std::string MOONG::Browser::REG_SUB_KEY_WINDOWS_DEFAULT_BROWSER = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts\\.htm\\UserChoice";
 const std::string MOONG::Browser::REG_VALUE_WINDOWS_DEFAULT_BROWSER = "ProgID";
 
-void MOONG::Browser::OpenURL(const std::string url, std::string browser/* = "Default"*/) noexcept(false)
+void MOONG::Browser::OpenURL(const std::string url, std::string browser/* = "Default"*/)
 {
-	try
-	{
-		std::transform(browser.begin(), browser.end(), browser.begin(), ::tolower);
+	std::transform(browser.begin(), browser.end(), browser.begin(), ::tolower);
 
-		if (browser.compare("default") == 0)
-		{
-			MOONG::Browser::OpenURLWithWindowsDefaultBrowser(url);
-		}
-		else if (browser.compare("ie") == 0)
-		{
-			MOONG::Browser::OpenURLWithIE(url);
-		}
-		else if (browser.compare("internet explorer") == 0)
-		{
-			MOONG::Browser::OpenURLWithIE(url);
-		}
-		else if (browser.compare("chrome") == 0)
-		{
-			MOONG::Browser::OpenURLWithChrome(url);
-		}
-		else if (browser.compare("edge") == 0)
-		{
-			MOONG::Browser::OpenURLWithMSEdge(url);
-		}
-		else if (browser.compare("msedge") == 0)
-		{
-			MOONG::Browser::OpenURLWithMSEdge(url);
-		}
-		else
-		{
-			MOONG::Browser::OpenURLWithWindowsDefaultBrowser(url);
-		}
-	}
-	catch (const std::exception& exception)
+	if (browser.compare("default") == 0)
 	{
-		throw exception;
+		MOONG::Browser::OpenURLWithWindowsDefaultBrowser(url);
+	}
+	else if (browser.compare("ie") == 0)
+	{
+		MOONG::Browser::OpenURLWithIE(url);
+	}
+	else if (browser.compare("internet explorer") == 0)
+	{
+		MOONG::Browser::OpenURLWithIE(url);
+	}
+	else if (browser.compare("chrome") == 0)
+	{
+		MOONG::Browser::OpenURLWithChrome(url);
+	}
+	else if (browser.compare("edge") == 0)
+	{
+		MOONG::Browser::OpenURLWithMSEdge(url);
+	}
+	else if (browser.compare("msedge") == 0)
+	{
+		MOONG::Browser::OpenURLWithMSEdge(url);
+	}
+	else
+	{
+		MOONG::Browser::OpenURLWithWindowsDefaultBrowser(url);
 	}
 }
 
-void MOONG::Browser::OpenURLWithWindowsDefaultBrowser(const std::string url) noexcept(false)
+void MOONG::Browser::OpenURLWithWindowsDefaultBrowser(const std::string url)
 {
-	try
+	std::string windows_default_browser;
+
+	MOONG::Browser::getWindowsDefaultBrowser(windows_default_browser);
+
+	std::transform(windows_default_browser.begin(), windows_default_browser.end(), windows_default_browser.begin(), ::tolower);
+
+	// 파라미터 url 앞에 "-kiosk"가 붙어서 올 경우 크롬, 엣지는 정상 동작하지만,
+	// 다른 브라우저의 경우 "-kiosk"가 아닐 경우 여기서 치환해준다.
+	if (windows_default_browser.find("chrome") != std::string::npos)
 	{
-		std::string windows_default_browser;
-
-		MOONG::Browser::getWindowsDefaultBrowser(windows_default_browser);
-
-		std::transform(windows_default_browser.begin(), windows_default_browser.end(), windows_default_browser.begin(), ::tolower);
-
-		// 파라미터 url 앞에 "-kiosk"가 붙어서 올 경우 크롬, 엣지는 정상 동작하지만,
-		// 다른 브라우저의 경우 "-kiosk"가 아닐 경우 여기서 치환해준다.
-		if (windows_default_browser.find("chrome") != std::string::npos)
-		{
-			MOONG::Browser::OpenURLWithChrome(url);
-		}
-		else if (windows_default_browser.find("msedge") != std::string::npos)
-		{
-			MOONG::Browser::OpenURLWithMSEdge(url);
-		}
-		else if (windows_default_browser.find("htmlfile") != std::string::npos)
-		{
-			MOONG::Browser::OpenURLWithIE(url);
-		}
-		else
-		{
-			ShellExecuteA(NULL, "open", url.c_str(), "", "", SW_SHOW);	// 구현되지 않은 브라우저의 경우.
-		}
+		MOONG::Browser::OpenURLWithChrome(url);
 	}
-	catch (const std::exception& exception)
+	else if (windows_default_browser.find("msedge") != std::string::npos)
 	{
-		throw exception;
+		MOONG::Browser::OpenURLWithMSEdge(url);
+	}
+	else if (windows_default_browser.find("htmlfile") != std::string::npos)
+	{
+		MOONG::Browser::OpenURLWithIE(url);
+	}
+	else
+	{
+		ShellExecuteA(NULL, "open", url.c_str(), "", "", SW_SHOW);	// 구현되지 않은 브라우저의 경우.
 	}
 }
 
@@ -107,14 +93,7 @@ void MOONG::Browser::OpenURLWithMSEdge(const std::string url)
 	ShellExecuteA(NULL, "open", "msedge.exe", url.c_str(), "", SW_SHOW);
 }
 
-LSTATUS MOONG::Browser::getWindowsDefaultBrowser(std::string& windows_default_browser) noexcept(false)
+LSTATUS MOONG::Browser::getWindowsDefaultBrowser(std::string& windows_default_browser)
 {
-	try
-	{
-		return MOONG::Registry::Read(HKEY_CURRENT_USER, MOONG::Browser::REG_SUB_KEY_WINDOWS_DEFAULT_BROWSER.c_str(), MOONG::Browser::REG_VALUE_WINDOWS_DEFAULT_BROWSER.c_str(), windows_default_browser);
-	}
-	catch (const std::exception& exception)
-	{
-		throw exception;
-	}
+	return MOONG::Registry::Read(HKEY_CURRENT_USER, MOONG::Browser::REG_SUB_KEY_WINDOWS_DEFAULT_BROWSER.c_str(), MOONG::Browser::REG_VALUE_WINDOWS_DEFAULT_BROWSER.c_str(), windows_default_browser);
 }
--- Browser/Test.cpp
+++ Browser/Test.cpp
@@ -2,29 +2,22 @@
 
 int main()
 {
-	try
-	{
-		// -kiosk 옵션의 경우 해당 브라우저가 하나도 실행중이지 않아야 키오스크 모드로 실행된다.
-		MOONG::Browser::OpenURL("www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURL("www.google.com", "edge");
-		//system("pause");
-		//MOONG::Browser::OpenURL("-kiosk www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURLWithChrome("www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURLWithChrome("-kiosk www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURLWithIE("www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURLWithMSEdge("www.google.com");
-		//system("pause");
-		//MOONG::Browser::OpenURLWithMSEdge("-kiosk www.google.com");
-	}
-	catch (const std::exception& exception)
-	{
-		std::cout << exception.what() << std::endl;
-	}
+	// -kiosk 옵션의 경우 해당 브라우저가 하나도 실행중이지 않아야 키오스크 모드로 실행된다.
+	MOONG::Browser::OpenURL("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURL("www.google.com", "edge");
+	//system("pause");
+	//MOONG::Browser::OpenURL("-kiosk www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithChrome("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithChrome("-kiosk www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithIE("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithMSEdge("www.google.com");
+	//system("pause");
+	//MOONG::Browser::OpenURLWithMSEdge("-kiosk www.google.com");
 
 	return EXIT_SUCCESS;
 }
